c50ebf76d17f36a4232a22c78201a30b638a54c9
patelshivam <shivampatel3650@gmail.com>
2024-03-28 10:39:38 +0530

    adduser and getuser routes are completed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 const db = require("../db.js");
 
-router.get("/user", (req, res, next) => { 
-    const sql = "SELECT * FROM AUTHOR";
+
+// ROUTE 1: Get All the user using: GET "/api/auth/user"
+
+router.get("/user", (req, res, next) => {
+  const sql = "SELECT * FROM AUTHOR";
+  try {
     db.query(sql, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
+      if (err) throw err;
+      return res.json(data);
     });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-module.exports = router; 
+
+// ROUTE 2: Add the user using: POST "/api/auth/adduser"
+
+router.post("/adduser", (req, res, next) => {
+  const { Author_ID, Lastname, Firstname, Email, City, Country } = req.body;
+
+  // Check if required fields are present in the request body
+  if (!Author_ID || !Lastname || !Firstname || !Email || !City || !Country) {
+    return res
+      .status(400)
+      .json({ error: "Missing required fields in request body" });
+  }
+
+  const sql =
+    "INSERT INTO AUTHOR (Author_ID, Lastname, Firstname, Email, City, Country) VALUES (?, ?, ?, ?, ?, ?)";
+
+  try {
+    db.query(
+      sql,
+      [Author_ID, Lastname, Firstname, Email, City, Country],
+      (err, result) => {
+        if (err) throw err;
+        return res.json({
+          message: "User inserted successfully",
+          newUser: { Author_ID, Lastname, Firstname, Email, City, Country },
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+module.exports = router;
