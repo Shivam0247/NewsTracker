2f9ec0985b1b8d102dcf1a2bf0ea2bb5d9c66df5
patelshivam <shivampatel3650@gmail.com>
2024-03-31 14:25:48 +0530

    favourite icon is added and logic of only add if it is login is applied.

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,26 +1,44 @@
-import React from "react";
-
-const NewsItem = (props)=> {
-    let { title, description, imageUrl, url, date, author,source,Badge} = props;
-    return (
-      <div>
-        <div className="card bg-dark">
-
-          <div style={{display:"flex",position:"absolute",justifyContent:"flex-end",right:"0",top:"-7px"}}>
-          <span className={`badge rounded-pill bg-${Badge}`} >
-            {source}
-          </span>
-          </div>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
-          <div className="card-body" style={{color: "white"}}>
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {!author ? "Unknown" : author} on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const NewsItem = (props) => {
+  let { title, description, imageUrl, url, date, author, source, Badge } =
+    props;
+  const [isStarClicked, setIsStarClicked] = useState(false);
+  const navigate = useNavigate();
+  const handleStarClick = () => {
+    const checkLogin = localStorage.getItem("token");
+    if(checkLogin){
+    setIsStarClicked(!isStarClicked);
+    }
+    else{
+      navigate("/login");
+    }
+  };
+  return (
+    <div>
+      <div className="card bg-dark">
+        <div
+          style={{
+            display: "flex",
+            position: "absolute",
+            justifyContent: "flex-end",
+            right: "0",
+            top: "-7px",
+          }}
+        >
+          <span className={`badge rounded-pill bg-${Badge}`}>{source}</span>
+        </div>
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body" style={{ color: "white" }}>
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <div className="favourite" style={{ display: "flex" }}>
             <a
               href={url}
               target="_blank"
@@ -29,10 +47,22 @@ const NewsItem = (props)=> {
             >
               Read more
             </a>
+
+            <i
+              className={isStarClicked ? "fi fi-sr-star" : "fi fi-rr-star"}
+              style={{
+                display: "flex",
+                margin: "7px 7px",
+                cursor: "pointer",
+                color: isStarClicked ? "gold" : "inherit",
+              }}
+              onClick={handleStarClick}
+            ></i>
           </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default NewsItem
+export default NewsItem;
