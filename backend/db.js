a367b5a0a607b0d29e87b97cc290804ffb619641
Shivam Patel <112711878+Shivam0247@users.noreply.github.com>
2025-08-05 10:10:28 +0530

    db.js Updated

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
     const db = mysql.createConnection({
-        host: "localhost", 
-        user: "root",
-        password: "12345678",
-        database: "NewsTracker"
+        host: process.env.HOST, 
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
     });
 
 
